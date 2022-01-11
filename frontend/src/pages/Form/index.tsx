35f745d982142b26b224098f5fb39e598bd147e8
Felipe Martins <fegabriel77skt@icloud.com>
2022-01-11 13:51:20 -0300

    Form layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,7 +1,42 @@
-function Form () {
+import './styles.css'
+
+function Form() {
+
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
 
     return (
-        <h1>Pagina de Formulario</h1>
+        <div className="AvaliaMovie-form-container">
+            <img className="AvaliaMovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="AvaliaMovie-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <form className="AvaliaMovie-form">
+                    <div className="form-group AvaliaMovie-form-group">
+                        <label htmlFor="email">Informe seu email</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="form-group AvaliaMovie-form-group">
+                        <label htmlFor="score">Informe sua avaliação</label>
+                        <select className="form-control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="AvaliaMovie-form-btn-container">
+                        <button type="submit" className="btn btn-primary AvaliaMovie-btn">Salvar</button>
+                    </div>
+                </form >
+                <button className="btn btn-primary AvaliaMovie-btn mt-3">Cancelar</button>
+            </div >
+        </div >
     )
 }
 
