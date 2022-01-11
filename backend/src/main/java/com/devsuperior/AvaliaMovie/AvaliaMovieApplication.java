ed49d0804dd1c4bdd14d8e7edc8b55450ece46e8
Felipe Martins <fegabriel77skt@icloud.com>
2022-01-11 11:04:35 -0300

    projet created

---
+++ backend/src/main/java/com/devsuperior/AvaliaMovie/AvaliaMovieApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.AvaliaMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliaMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliaMovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/AvaliaMovie/AvaliaMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.AvaliaMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliaMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
